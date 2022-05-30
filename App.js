a30ef2162c83c7cc471006b7cc04c2589a037a22
Carlos Alexandre Ferreira de Oliveira <52263150+CarlosAlexFO@users.noreply.github.com>
2022-05-30 17:33:04 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useFonts,
+   Montserrat_400Regular, 
+  Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+  import AppLoading from 'expo-app-loading';
+  import Cesta from './src/telas/Cesta';
+  import mock from './src/telas/mocks/cesta';
+
+export default function App() {
+const [fonteCarregada] = useFonts({
+"MontSerratRegular": Montserrat_400Regular,
+"MontSerratBold": Montserrat_700Bold,
+});
+if (!fonteCarregada){
+  return <AppLoading />
+}
+
+  return (
+      <SafeAreaView style={{flex: 1}}>
+        <StatusBar / >
+     <Cesta {...mock} />
+   </SafeAreaView>
+  );
+}
+
+
